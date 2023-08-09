7f7b154b60c94b8ce560f2f6592bfcc3a3b1a797
soumen panuya <soumenpanuya86@gmail.com>
2023-08-09 11:25:57 +0530

    remove route file

--- routes/patient_route.js
+++
@@ -1,9 +0,0 @@
-const express =require("express");
-
-const router =express.Router();
-const patient_controller = require("../controller/patient_controller");
-
-router.post("/registation",patient_controller.registation);
-
-
-module.exports =router;
